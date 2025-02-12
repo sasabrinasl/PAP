76a5853c3867c66b3599b6b75649c95593ffa238
Sabrina Silvestre da Luz <sabrinasilvestre279@gmail.com>
2025-02-12 11:42:27 +0000

    add flask

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function goToPage(pageId) {
     // Esconde todas as páginas
-    document.querySelectorAll('.container > div').forEach(page => {
+    document.querySelectorAll('body > div:not(.header)').forEach(page => {
         page.style.display = 'none';
     });
 
     // Mostra a página selecionada
     document.getElementById(pageId).style.display = 'block';
 }
+
+
+        
+        
